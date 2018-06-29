a493284fb484536bd91959d2e29b42871997d27c
ritabc <ritabc@gmail.com>
2018-06-29 10:55:56 -0700

    Fail test 2 - check for anagram

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -7,4 +7,8 @@ describe ('Phrase#check_for_anagram') do
     phrase = Phrase.new("ruby")
     expect(phrase.get_receiver()).to(eq("ruby"))
   end
+  it ('takes the receiver and the argument and checks for an anagram') do
+    phrase = Phrase.new("ruby")
+    expect(phrase.check_for_anagram("bury")).to(eq("That is an anagram!"))
+  end
 end
