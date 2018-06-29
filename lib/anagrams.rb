5e764036d91e546f0718acac9eb5d89ffa81d2e8
ritabc <ritabc@gmail.com>
2018-06-29 11:30:58 -0700

    Research array methods

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -8,7 +8,10 @@ class Phrase
     @main_phrase
   end
 
-  # def check_for_anagram
-  # end
+  def check_for_anagram(argument)
+    ## convert string to array of letters
+    ## sort array of letters - .sort
+    ## compare array of letters using include
+  end
 
 end
